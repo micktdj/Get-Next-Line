f10c33a3ad81f7ce70436d5ef50e01ef65620d77
Mickael TORDJMAN <mtordjma@e3r10p20.42.fr>
2019-03-18 21:05:46 +0100

    add

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtordjma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/27 12:21:52 by mtordjma          #+#    #+#             */
+/*   Updated: 2018/12/24 13:28:28 by mtordjma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "./libft/libft.h"
+# include <unistd.h>
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
